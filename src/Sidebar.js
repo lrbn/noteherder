24e5cc7cf1273d674efa5013536e5bb280b73495
brandon <lrbn86@gmail.com>
2017-06-14 17:44:53 -0400

    Need to find a way to add note to NoteList

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,13 +6,31 @@ import new_ from './new.png'
 
 import './Sidebar.css'
 
+function addNote(ev) {
+  console.log("Added a note.")
+    // Find a way add to NoteList
+    // Layout of a Note
+    // < li >
+    //  <div className="note">
+    //    <div className="note-title">
+    //      Note Title
+    //    </div>
+    //    <div className="note-body">
+    //      <p>
+    //        Note Content
+    //      </p>
+    //    </div>
+    //  </div>
+    // </li >
+}
+
 const Sidebar = () => {
   return (
     <div className="Sidebar">
       <div className="logo">
         <img src={quill} alt="Noteherder" />
       </div>
-      <button className="new-note">
+      <button className="new-note" onClick={addNote}>
         <img src={new_hover} alt="New note" />
         <img className="outline" src={new_} alt="New note" />
       </button>
