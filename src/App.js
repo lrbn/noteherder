436f818c3bffe66423a7b3d421f1775d5e47b009
brandon <lrbn86@gmail.com>
2017-06-15 11:33:31 -0400

    Handle submit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      notes: {
-        'note-1': {
-          id: 'note-1',
-          title: 'Thoughts on React',
-          body: 'React is pretty nifty. Declarative FTW',
-        },
-        'note-2': {
-          id: 'note-2',
-          title: 'State and props?',
-          body: 'Wat',
-        },
-      },
+      notes: {},
     }
   }
 
--- src/NoteForm.js
+++ src/NoteForm.js
@@ -29,10 +29,15 @@ class NoteForm extends Component {
     )
   }
 
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.setState({ note: this.blankNote() })
+  }
+
   render() {
     return (
       <div className="NoteForm">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <p>
             <input
               type="text"
@@ -49,6 +54,7 @@ class NoteForm extends Component {
               onChange={this.handleChanges}
               value={this.state.note.body}
             ></textarea>
+            <button type="submit">Save and new</button>
           </p>
         </form>
       </div>
